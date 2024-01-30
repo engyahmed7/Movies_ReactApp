22ab0e07e137c64e2cb999c0d869fc64abd653f7
Engy Ahmed Abdelaziz <engya306@gmail.com>
2024-01-30 11:23:03 +0200

    Refactor Login and Add Register Component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,30 +1,30 @@
-import axios from 'axios';
-import React, { useState } from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 export default function Login() {
-
-  const [error, setError] = useState('')
+  const [error, setError] = useState("");
   const [user, setUser] = useState({
-    email:"",password:""
+    email: "",
+    password: "",
   });
 
-
-  function getUser(e){
-    let myuser = {...user};
+  function getUser(e) {
+    let myuser = { ...user };
     myuser[e.target.name] = e.target.value;
     setUser(myuser);
   }
 
-  async function submitForm(e){
+  async function submitForm(e) {
     e.preventDefault();
     try {
-
-      let {data}= await axios.post(`http://localhost:3001/v1/auth/login`,user);
+      let { data } = await axios.post(
+        `http://localhost:3001/v1/auth/login`,
+        user
+      );
       console.log(data);
-      if(data.message === "user exist"){
+      if (data.message === "user exist") {
         window.location.href = "/home";
-      }
-      else{
+      } else {
         setError(data.message);
         console.log(error.message);
       }
@@ -36,19 +36,33 @@ export default function Login() {
 
   return (
     <div className="w-75 mx-auto py-3">
-        <h1 className="pb-3">Login</h1>
-        <form onSubmit={submitForm}>
-          <div className="alert alert-danger"> {error}</div>
-          <div className="px-3">
-              <label htmlFor="email"></label>
-              <input type="email" placeholder='email' name="email" className="form-control" onChange={getUser} />
-          </div>
-          <div className="px-3">
-              <label htmlFor="password"></label>
-              <input type="password" placeholder='password' name="password" className="form-control" onChange={getUser} />
-          </div>
-          <button type="submit" className="btn btn-info m-3">LOGIN</button>
-        </form>
-      </div>
-  )
+      <h1 className="pb-3">Login</h1>
+      <form onSubmit={submitForm}>
+        <div className="alert alert-danger"> {error}</div>
+        <div className="px-3">
+          <label htmlFor="email"></label>
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            className="form-control"
+            onChange={getUser}
+          />
+        </div>
+        <div className="px-3">
+          <label htmlFor="password"></label>
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            className="form-control"
+            onChange={getUser}
+          />
+        </div>
+        <button type="submit" className="btn btn-info m-3">
+          LOGIN
+        </button>
+      </form>
+    </div>
+  );
 }
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,108 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+export default function Register() {
+  const [error, setError] = useState("");
+
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+    phoneNumber: "",
+  });
+
+  function getUser(e) {
+    let myuser = { ...user };
+    myuser[e.target.name] = e.target.value;
+    setUser(myuser);
+    console.log(myuser);
+  }
+
+  async function formSubmit(e) {
+    e.preventDefault();
+  
+    try {
+      let { data } = await axios.post(`http://localhost:3001/v1/auth/register`, user);
+  
+      if (data.role === 'user') {
+        window.location.href = "/login";
+      } else {
+        setError(data.message);
+        console.log(error.message);
+      }
+    } catch (error) {
+      console.error("An error occurred:", error.response.data.message);
+      setError(error.response.data.message);
+    }
+  }
+  
+  return (
+    <>
+      <div className="w-75 mx-auto py-4">
+        <h1 className="pb-3">Register Now</h1>
+
+        <form onSubmit={formSubmit}>
+          {/* {error && <div className="alert alert-danger"></div>} */}
+          <div className="alert alert-danger"> {error}</div>
+          <div className="my-3">
+            <label htmlFor="first-name pb-2"></label>
+            <input
+              type="text"
+              onChange={getUser}
+              placeholder="first name"
+              name="name"
+              className="form-control"
+            />
+          </div>
+          <div className="my-3">
+            <label htmlFor="email pb-2"></label>
+            <input
+              type="email"
+              onChange={getUser}
+              placeholder="email"
+              name="email"
+              className="form-control"
+            />
+          </div>
+
+          <div className="my-3">
+            <label htmlFor="password pb-2"></label>
+            <input
+              type="password"
+              onChange={getUser}
+              placeholder="password"
+              name="password"
+              className="form-control"
+            />
+          </div>
+          <div className="my-3">
+            <label htmlFor="password pb-2"></label>
+            <input
+              type="password"
+              onChange={getUser}
+              placeholder="Confirm password"
+              name="confirmpassword"
+              className="form-control"
+            />
+          </div>
+          <div className="my-3">
+            <label htmlFor="last-name pb-2"></label>
+            <input
+              type="text"
+              onChange={getUser}
+              placeholder="phoneNumber"
+              name="phoneNumber"
+              className="form-control"
+            />
+          </div>
+
+          <button type="submit" className="btn btn-info mt-3 ">
+            {" "}
+            Register
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
